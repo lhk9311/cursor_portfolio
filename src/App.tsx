82e7e9436d4a7e81ad2a906b28c62cbc597c7895
lhk9311 <lhk9311@gmail.com>
2026-05-13 18:18:28 +0900

    test hero component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import Hero from './components/Hero/Hero.jsx'
 import './App.css'
 
 export default function App() {
   return (
-    <>
-      <Hero />
-      {/* 스크롤 구간 확보용 (ScrollTrigger 테스트) */}
-      <section className="scroll-spacer" aria-hidden="true" />
-    </>
+    <div style={{ color: 'black', fontSize: '50px' }}>
+      HELLO NETLIFY
+    </div>
   )
 }
